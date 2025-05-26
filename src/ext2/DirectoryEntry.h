3793538edcc940f6944b22e1ea08cea203c80f92
JivkoNushev <jivkon1410@gmail.com>
2025-05-27 00:55:34 +0300

    [Impl] Added a directory entry structure

--- src/ext2/DirectoryEntry.h
+++ src/ext2/DirectoryEntry.h
@@ -2,7 +2,22 @@
 
 #include <stdint.h>
 
-struct DirectoryEntry {};
+struct DirectoryEntry
+{
+public:
+    enum FileType : uint8_t
+    {
+        EXT2_FT_UNKNOWN     = 0, // Unknown File Type
+        EXT2_FT_REG_FILE    = 1, // Regular File
+        EXT2_FT_DIR         = 2, // Directory File
+        EXT2_FT_CHRDEV      = 3, // Character Device
+        EXT2_FT_BLKDEV      = 4, // Block Device
+        EXT2_FT_FIFO        = 5, // Buffer File
+        EXT2_FT_SOCK        = 6, // Socket File
+        EXT2_FT_SYMLINK     = 7  // Symbolic Link
+    };
+
+};
 
 struct LinkedDirectoryEntry : public DirectoryEntry
 {
